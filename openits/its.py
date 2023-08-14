7c9fcb9cb6becefa5f395b4c0f6d71982365b0b9
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2023-08-15 01:02:54 +0800

    Update its.py

--- openits/its.py
+++ openits/its.py
@@ -20,13 +20,15 @@ class ITSLangevinIntegratorGenerator:
         log_nk: List[float] = None,
         log_nk2: List[float] = None,
         its_log: str = None,
-        boost_group: int = EnhancedGroup.ALL
+        boost_group: int = EnhancedGroup.ALL,
+        verbose: bool = False
     ):
         """Initialize the ITS Langevin Integrator"""
         
         self.friction = friction
         self.dt = dt
         self.integrator = None
+        self.verbose = verbose
 
         if its_log is not None:
             temperature_list, log_nk, log_nk2 = self.load_log(its_log)
@@ -52,6 +54,26 @@ class ITSLangevinIntegratorGenerator:
         self.boost_group = boost_group
         self.set_integrator(boost_group=boost_group)
 
+    def integ_addGlobalVariable(self, name, value):
+        if self.verbose:
+            print(f">>> global_init: {name} = {value}")
+        self.integrator.addGlobalVariable(name, value)
+
+    def integ_addPerDofVariable(self, name, value):
+        if self.verbose:
+            print(f">>> per_dof_init: {name} = {value}")
+        self.integrator.addPerDofVariable(name, value)
+
+    def integ_addComputeGlobal(self, name, value):
+        if self.verbose:
+            print(f">>> global_set: {name} = {value}")
+        self.integrator.addComputeGlobal(name, value)
+    
+    def integ_addComputePerDof(self, name, value):
+        if self.verbose:
+            print(f">>> per_dof_set: {name} = {value}")
+        self.integrator.addComputePerDof(name, value)
+
     def set_integrator(self, boost_group: int = None):
         """Set the ITS Langevin Integrator"""
         if boost_group is None:
@@ -63,142 +85,136 @@ class ITSLangevinIntegratorGenerator:
         kB = 8.314 / 1000.0  # J/mol/K
         kT = kB * temperature
 
-        self.integrator = mm.CustomIntegrator(dt)
+        self.integrator = mm.CustomIntegrator(self.dt)
         self.integrator.setConstraintTolerance(1e-5)
-        self.integrator.addGlobalVariable("a", np.exp(-friction * dt))
-        self.integrator.addGlobalVariable("b", np.sqrt(1 - np.exp(-2 * friction * dt)))
-        self.integrator.addGlobalVariable("one_beta", kT)
-        self.integrator.addPerDofVariable("x1", 0.0)
+        self.integ_addGlobalVariable("a", np.exp(-friction * dt))
+        self.integ_addGlobalVariable("b", np.sqrt(1 - np.exp(-2 * friction * dt)))
+        self.integ_addGlobalVariable("one_beta", kT)
+        self.integ_addPerDofVariable("x1", 0.0)
 
         if boost_group == EnhancedGroup.E1 or boost_group == EnhancedGroup.ALL:
-            self.integrator.addGlobalVariable("vmax", 0.0)
-            self.integrator.addGlobalVariable("Aup", 0.0)
-            self.integrator.addGlobalVariable("Adown", 0.0)
-            self.integrator.addGlobalVariable("A", 0.0)
+            self.integ_addGlobalVariable("vmax", 0.0)
+            self.integ_addGlobalVariable("Aup", 0.0)
+            self.integ_addGlobalVariable("Adown", 0.0)
+            self.integ_addGlobalVariable("A", 0.0)
             for i in range(len(self.temperature_list)):
-                self.integrator.addGlobalVariable(f"hyper_e_{i}", 0.0)
+                self.integ_addGlobalVariable(f"hyper_e_{i}", 0.0)
             for i in range(len(self.temperature_list)-1):
-                self.integrator.addGlobalVariable(f"max_{i}", 0.0)
+                self.integ_addGlobalVariable(f"max_{i}", 0.0)
 
             # calc A
             A_up, A_down, vmax = [], [], []
             for i in range(len(self.temperature_list)):
                 temp = self.temperature_list[i]
                 log_nk = self.log_nk[i]
-                beta_k = 1 / (kB * temp)
-                beta_k_over_beta_0 = temp / temperature
                 
                 if boost_group == EnhancedGroup.E1:
-                    self.integrator.addComputeGlobal(f"hyper_e_{i}", f"-{beta_k:.12e}*energy1+{log_nk:.12e}")
+                    self.integ_addComputeGlobal(f"hyper_e_{i}", f"-1/{kB * temp:.12e}*energy1+{log_nk:.12e}")
                 elif boost_group == EnhancedGroup.ALL:
-                    self.integrator.addComputeGlobal(f"hyper_e_{i}", f"-{beta_k:.12e}*energy+{log_nk:.12e}")
+                    self.integ_addComputeGlobal(f"hyper_e_{i}", f"-1/{kB * temp:.12e}*energy+{log_nk:.12e}")
                 A_up.append(
-                    f"{beta_k_over_beta_0:.12e}*exp(hyper_e_{i}-vmax)"
+                    f"{temperature:.12e}/{temp:.12e}*exp(hyper_e_{i}-vmax)"
                 )
                 A_down.append(f"exp(hyper_e_{i}-vmax)")
                 vmax.append(f"hyper_e_{i}")
 
             for nmax in range(len(vmax)-1):
                 if nmax == 0:
-                    self.integrator.addComputeGlobal(f"max_{nmax}", f"max({vmax[nmax]}, {vmax[nmax+1]})")
+                    self.integ_addComputeGlobal(f"max_{nmax}", f"max({vmax[nmax]}, {vmax[nmax+1]})")
                 else:
-                    self.integrator.addComputeGlobal(f"max_{nmax}", f"max(max_{nmax-1}, {vmax[nmax+1]})")
+                    self.integ_addComputeGlobal(f"max_{nmax}", f"max(max_{nmax-1}, {vmax[nmax+1]})")
 
-            self.integrator.addComputeGlobal("vmax", f"max_{len(vmax)-2}")
-            self.integrator.addComputeGlobal("Aup", "+".join(A_up))
-            self.integrator.addComputeGlobal("Adown", "+".join(A_down))
-            self.integrator.addComputeGlobal("A", "Aup/Adown")
-            self.integrator.addUpdateContextState()
+            self.integ_addComputeGlobal("vmax", f"max_{len(vmax)-2}")
+            self.integ_addComputeGlobal("Aup", "+".join(A_up))
+            self.integ_addComputeGlobal("Adown", "+".join(A_down))
+            self.integ_addComputeGlobal("A", "Aup/Adown")
+            self.integ_addUpdateContextState()
             if boost_group == EnhancedGroup.E1:
-                self.integrator.addComputePerDof("v", "v + dt*A*f1/m")
-                self.integrator.addComputePerDof("v", "v + dt*f0/m")
+                self.integ_addComputePerDof("v", "v + dt*A*f1/m")
+                self.integ_addComputePerDof("v", "v + dt*f0/m")
             else:
-                self.integrator.addComputePerDof("v", "v + dt*A*f/m")
+                self.integ_addComputePerDof("v", "v + dt*A*f/m")
 
         elif boost_group == EnhancedGroup.E1_AND_E2:
-            self.integrator.addGlobalVariable("vmax_1", 0.0)
-            self.integrator.addGlobalVariable("Aup_1", 0.0)
-            self.integrator.addGlobalVariable("Adown_1", 0.0)
-            self.integrator.addGlobalVariable("A_1", 0.0)
-            self.integrator.addGlobalVariable("vmax_2", 0.0)
-            self.integrator.addGlobalVariable("Aup_2", 0.0)
-            self.integrator.addGlobalVariable("Adown_2", 0.0)
-            self.integrator.addGlobalVariable("A_2", 0.0)
+            self.integ_addGlobalVariable("vmax_1", 0.0)
+            self.integ_addGlobalVariable("Aup_1", 0.0)
+            self.integ_addGlobalVariable("Adown_1", 0.0)
+            self.integ_addGlobalVariable("A_1", 0.0)
+            self.integ_addGlobalVariable("vmax_2", 0.0)
+            self.integ_addGlobalVariable("Aup_2", 0.0)
+            self.integ_addGlobalVariable("Adown_2", 0.0)
+            self.integ_addGlobalVariable("A_2", 0.0)
             # prepare for E1
             for i in range(len(self.temperature_list)):
-                self.integrator.addGlobalVariable(f"hyper_e_{i}_1", 0.0)
+                self.integ_addGlobalVariable(f"hyper_e_{i}_1", 0.0)
             for i in range(len(self.temperature_list)-1):
-                self.integrator.addGlobalVariable(f"max_{i}_1", 0.0)
+                self.integ_addGlobalVariable(f"max_{i}_1", 0.0)
             # prepare for E2
             for i in range(len(self.temperature_list)):
-                self.integrator.addGlobalVariable(f"hyper_e_{i}_2", 0.0)
+                self.integ_addGlobalVariable(f"hyper_e_{i}_2", 0.0)
             for i in range(len(self.temperature_list)-1):
-                self.integrator.addGlobalVariable(f"max_{i}_2", 0.0)
+                self.integ_addGlobalVariable(f"max_{i}_2", 0.0)
 
             # calc A1
             A_up_1, A_down_1, vmax_1 = [], [], []
             for i in range(len(self.temperature_list)):
                 temp = self.temperature_list[i]
                 log_nk = self.log_nk[i]
-                beta_k = 1 / (kB * temp)
-                beta_k_over_beta_0 = temp / temperature
                 
-                self.integrator.addComputeGlobal(f"hyper_e_{i}_1", f"-{beta_k:.12e}*energy1+{log_nk:.12e}")
+                self.integ_addComputeGlobal(f"hyper_e_{i}_1", f"-1/{kB * temp:.12e}*energy1+{log_nk:.12e}")
                 A_up_1.append(
-                    f"{beta_k_over_beta_0:.12e}*exp(hyper_e_{i}_1-vmax_1)"
+                    f"{temperature:.12e}/{temp:.12e}*exp(hyper_e_{i}_1-vmax_1)"
                 )
                 A_down_1.append(f"exp(hyper_e_{i}_1-vmax_1)")
                 vmax_1.append(f"hyper_e_{i}_1")
 
             for nmax in range(len(vmax_1)-1):
                 if nmax == 0:
-                    self.integrator.addComputeGlobal(f"max_{nmax}_1", f"max({vmax_1[nmax]}, {vmax_1[nmax+1]})")
+                    self.integ_addComputeGlobal(f"max_{nmax}_1", f"max({vmax_1[nmax]}, {vmax_1[nmax+1]})")
                 else:
-                    self.integrator.addComputeGlobal(f"max_{nmax}_1", f"max(max_{nmax-1}_1, {vmax_1[nmax+1]})")
+                    self.integ_addComputeGlobal(f"max_{nmax}_1", f"max(max_{nmax-1}_1, {vmax_1[nmax+1]})")
 
             # calc A2
             A_up_2, A_down_2, vmax_2 = [], [], []
             for i in range(len(self.temperature_list)):
                 temp = self.temperature_list[i]
                 log_nk = self.log_nk2[i]
-                beta_k = 1 / (kB * temp)
-                beta_k_over_beta_0 = temp / temperature
                 
-                self.integrator.addComputeGlobal(f"hyper_e_{i}_2", f"-{beta_k:.12e}*energy2+{log_nk:.12e}")
+                self.integ_addComputeGlobal(f"hyper_e_{i}_2", f"-1/{kB * temp:.12e}*energy2+{log_nk:.12e}")
                 A_up_2.append(
-                    f"{beta_k_over_beta_0:.12e}*exp(hyper_e_{i}_2-vmax_2)"
+                    f"{temperature:.12e}/{temp:.12e}*exp(hyper_e_{i}_2-vmax_2)"
                 )
                 A_down_2.append(f"exp(hyper_e_{i}_2-vmax_2)")
                 vmax_2.append(f"hyper_e_{i}_2")
             
             for nmax in range(len(vmax_2)-1):
                 if nmax == 0:
-                    self.integrator.addComputeGlobal(f"max_{nmax}_2", f"max({vmax_2[nmax]}, {vmax_2[nmax+1]})")
+                    self.integ_addComputeGlobal(f"max_{nmax}_2", f"max({vmax_2[nmax]}, {vmax_2[nmax+1]})")
                 else:
-                    self.integrator.addComputeGlobal(f"max_{nmax}_2", f"max(max_{nmax-1}_2, {vmax_2[nmax+1]})")
+                    self.integ_addComputeGlobal(f"max_{nmax}_2", f"max(max_{nmax-1}_2, {vmax_2[nmax+1]})")
 
-            self.integrator.addComputeGlobal("vmax_1", f"max_{len(vmax_1)-2}_1")
-            self.integrator.addComputeGlobal("Aup_1", "+".join(A_up_1))
-            self.integrator.addComputeGlobal("Adown_1", "+".join(A_down_1))
-            self.integrator.addComputeGlobal("A_1", "Aup_1/Adown_1")
+            self.integ_addComputeGlobal("vmax_1", f"max_{len(vmax_1)-2}_1")
+            self.integ_addComputeGlobal("Aup_1", "+".join(A_up_1))
+            self.integ_addComputeGlobal("Adown_1", "+".join(A_down_1))
+            self.integ_addComputeGlobal("A_1", "Aup_1/Adown_1")
 
-            self.integrator.addComputeGlobal("vmax_2", f"max_{len(vmax_2)-2}_2")
-            self.integrator.addComputeGlobal("Aup_2", "+".join(A_up_2))
-            self.integrator.addComputeGlobal("Adown_2", "+".join(A_down_2))
-            self.integrator.addComputeGlobal("A_2", "Aup_2/Adown_2")
+            self.integ_addComputeGlobal("vmax_2", f"max_{len(vmax_2)-2}_2")
+            self.integ_addComputeGlobal("Aup_2", "+".join(A_up_2))
+            self.integ_addComputeGlobal("Adown_2", "+".join(A_down_2))
+            self.integ_addComputeGlobal("A_2", "Aup_2/Adown_2")
 
             self.integrator.addUpdateContextState()
-            self.integrator.addComputePerDof("v", "v + dt*f0/m")
-            self.integrator.addComputePerDof("v", "v + dt*A_1*f1/m")
-            self.integrator.addComputePerDof("v", "v + dt*A_2*f2/m")
+            self.integ_addComputePerDof("v", "v + dt*f0/m")
+            self.integ_addComputePerDof("v", "v + dt*A_1*f1/m")
+            self.integ_addComputePerDof("v", "v + dt*A_2*f2/m")
 
         self.integrator.addConstrainVelocities()
-        self.integrator.addComputePerDof("x", "x + 0.5*dt*v")
-        self.integrator.addComputePerDof("v", "a*v + b*sqrt(one_beta/m)*gaussian")
-        self.integrator.addComputePerDof("x", "x + 0.5*dt*v")
-        self.integrator.addComputePerDof("x1", "x")
+        self.integ_addComputePerDof("x", "x + 0.5*dt*v")
+        self.integ_addComputePerDof("v", "a*v + b*sqrt(one_beta/m)*gaussian")
+        self.integ_addComputePerDof("x", "x + 0.5*dt*v")
+        self.integ_addComputePerDof("x1", "x")
         self.integrator.addConstrainPositions()
-        self.integrator.addComputePerDof("v", "v + (x-x1)/dt")
+        self.integ_addComputePerDof("v", "v + (x-x1)/dt")
 
     def update_nk(self, energies, energies_2 = None, ratio=0.5):
         # calculate new nk from energies
